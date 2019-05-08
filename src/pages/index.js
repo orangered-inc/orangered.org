ad023a0beb157c30d5b7d31f7c381ec26fafc708
Gerald Monaco <gerald@gmonaco.me>
2019-05-07 19:39:06 -0700

    Initial commit

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,3 +1,16 @@
 import React from "react"
+import logo from "./orangered.png"
+import { Helmet } from "react-helmet"
 
-export default () => <div>Hello world!</div>
+export default () => (
+  <div style={{ textAlign: "center" }}>
+    <Helmet>
+      <meta charSet="utf-8" />
+      <title>Orangered, Inc</title>
+      <link rel="canonical" href="https://www.orangered.org" />
+    </Helmet>
+    <a href="mailto:hello@orangered.org">
+      <img style={{ width: "40%" }} src={logo} alt="Orangered" />
+    </a>
+  </div>
+)
